6a00ae34a1d7951195d8e7ac853e7dcacf9c24d8
cmenseau <cyrielle.menseau@insa-lyon.fr>
2020-01-22 17:33:55 +0100

    ajout timeline chronologique et requete adaptée sparql

--- js/autocomplete.js
+++ js/autocomplete.js
@@ -1,13 +1,34 @@
+$(document).ready(function(){
+  let slider = document.getElementById("myRange");
+  let output = document.getElementById("dateOutput");
+  output.value = slider.value;
+  autoComplete();
+
+  slider.oninput = function() {
+    output.value = this.value;
+    autoComplete();
+  };
+
+  output.onchange = function() {
+    slider.value = output.value;
+  };
+});
+
+
 function autoComplete() {
   var baseURL = 'https://dbpedia.org/sparql?default-graph-uri=http%3A%2F%2Fdbpedia.org&query=';
+
+  let slider = document.getElementById("myRange");
 
   var autoCompleteQuery = `
   SELECT DISTINCT ?planet ?label
   WHERE {
   ?planet a dbo:Planet.
   ?planet foaf:name ?label.
+  ?planet dbo:discovered ?date.
+  FILTER (?date < "`+slider.value+`-01-01"^^xsd:date)
   }
-  LIMIT 1000
+  LIMIT 3000
   `;
   var queryParams = '&format=application%2Fsparql-results%2Bjson&CXML_redir_for_subjs=121&CXML_redir_for_hrefs=&timeout=30000&debug=on&run=+Run+Query+';
   var autoCompleteQuery = baseURL + encodeURI(autoCompleteQuery) + queryParams;
@@ -26,7 +47,9 @@ function autoComplete() {
         if (a < b) { return -1; }
         if (a > b) { return 1; }
         return 0;
-      })
+      });
+
+      document.getElementById("planetNames").innerHTML = ""; // remove previous planets/children
 
       for (let i = 0; i < planetNames.length; i++) {
         var node = document.createElement("OPTION");
@@ -34,6 +57,9 @@ function autoComplete() {
         node.appendChild(textnode);
         document.getElementById("planetNames").appendChild(node);
       }
+      console.log(planetNames);
+
+      document.getElementById('nbObject').innerHTML = planets.length;
     },
     error: function(error) {
       console.log("Error during autoComplete")
